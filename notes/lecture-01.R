5e95dccf054a6c3932955feaf2255f09cb2b93bb
jhcreed <jordan.h.creed@moffitt.org>
2022-05-09 12:01:24 -0400

    live coding notes

---
+++ notes/lecture-01.R
@@ -0,0 +1,10 @@
+##### installing and using packages 
+
+##### working directory
+# here package and tidyverse
+
+##### data types and structures
+# types: logical, numerical, character
+# structures: vector, list, matrix, dataframe/tibble
+
+
